a5a7163d5236a9e4fa8210535eff292ef7d3b596
HaSeeb <hasibpanjshery2@gmail.com>
2023-09-08 11:45:44 +0500

    portfolio files

---
+++ script.js
@@ -0,0 +1,17 @@
+function scrollNext(){
+        document.querySelector(".cards").scrollBy(450, 0);
+    }
+    function scrollPrev(){
+        document.querySelector(".cards").scrollBy(-450, 0);
+    }   
+    
+// Android Projects Scroll
+    function scrollNextAnd(){
+        document.querySelector(".android-cards").scrollBy(450, 0);
+
+    }
+
+    function scrollPrevAnd(){
+        document.querySelector(".android-cards").scrollBy(-450, 0);
+        
+    }
