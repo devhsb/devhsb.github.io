47f5511cb53428b13fc7e64ba5841b2750412536
HaSeeb <hasibpanjshery2@gmail.com>
2023-09-09 13:27:20 +0500

    script

--- script.js
+++ script.js
@@ -15,3 +15,58 @@ function scrollNext(){
         document.querySelector(".android-cards").scrollBy(-450, 0);
         
     }
+
+//scroll animation
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const elements = document.querySelectorAll(".hide");
+elements.forEach((el) => observer.observe(el));
+
+
+//Skills animate
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const words = ["Java", "Android", "Web"];
+let interval = null;
+let wordIndex = 0;
+
+const animate = () => {
+  let iteration = 0;
+  const target = document.querySelector(".skill_typing");
+  
+  target.innerHTML = words[wordIndex]
+  target.dataset.value = words[wordIndex];
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    target.innerHTML = target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return `<span style="color: #009e66;">${target.dataset.value[index]}</span>`;
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= target.dataset.value.length){ 
+      clearInterval(interval);
+      wordIndex = (wordIndex + 1) % words.length;
+      setTimeout(animate, 1500);
+
+    }
+    
+    iteration += 1 / 3;
+  }, 70);
+}
+
+animate();
